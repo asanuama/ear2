ebc065119a91b48572bc637f484703746cdcf1db
EC2 Default User <you@example.com>
2020-11-25 08:52:55 +0000

    [update]user,product/index,show,editレイアウト修正

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '428521bf81f49cc21c5f434b3dc8bc6e72f93f56c8d9025eee9b9c3de93586a42e1a20fd3e426bcc66d96b2516309c0e8942a864a098e0a97626ffaf8a6d0603'
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :show, :edit, :update]
   resources :products do
-   resources :favorites, only: [:index, :create, :destroy]
+   resource :favorites, only: [:create, :destroy]
   end
   get 'user/my_page' => 'users#my_page' , as: 'user_my_page'
   get 'user/mode_select/:id' => 'users#mode_select', as: 'user_mode_select'
